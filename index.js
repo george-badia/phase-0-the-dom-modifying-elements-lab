7823d89f3c4bb9e6b4dcd33c2baccb33a96037c4
jojo <georgebadia2019@gmail.com>
2024-07-07 23:07:18 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const newMain = document.querySelector("main#main");
+newMain.remove();
+//creates a new h1
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'JOJO is the champion';
+
+document.body.appendChild(newHeader);
